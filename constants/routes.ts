632ac0d88b08343e1e77c4eae5a0ea7a45875f22
sourabhmandal <19mandal97@gmail.com>
2022-03-28 16:42:03 +0530

    remove unnecessary envs

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,21 +1,18 @@
 export const backendRoute = {
-  GOOGLE_LOGIN: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/auth/google/login`,
-  REFRESH_TOKEN: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/auth/token/refresh`
-};
-export const frontendRoute = {
-  DASHBOARD: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/quiz`,
-  ERROR_PAGE: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/404`,
-  LANDING: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/`,
-  DASHBOARD_JOBS: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/dashboard/jobs`,
-  THEMES: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/themes`,
-  CALANDER: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/calander`,
-  APPLICANT: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/applicants`,
-  SETTINGS: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/settings`,
-  HELLO_API: `${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/api/hello`,
-
   // oauth
   LOGIN: `/api/auth/login`,
   LOGOUT: `/api/auth/logout`,
   CALLBACK: `/api/auth/callback`,
   PROFILE: `/api/auth/me`
+};
+export const frontendRoute = {
+  DASHBOARD: `/quiz`,
+  ERROR_PAGE: `/404`,
+  LANDING: `/`,
+  DASHBOARD_JOBS: `/dashboard/jobs`,
+  THEMES: `/themes`,
+  CALANDER: `/calander`,
+  APPLICANT: `/applicants`,
+  SETTINGS: `/settings`,
+  HELLO_API: `/api/hello`
 };
