f8b76cfc5db4d85868a0264f85403a2c2c381b52
sourabhmandal <19mandal97@gmail.com>
2022-03-28 17:32:14 +0530

    add server function randomquizid

--- pages/api/randomquizid.ts
+++ pages/api/randomquizid.ts
@@ -4,7 +4,14 @@ export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+
+  const { min, max } = request.body;
+  let id_list: [number] = [Math.floor(Math.random() * (max - min + 1)) + min];
+  for (let i = 0; i < 9; i++) {
+    id_list.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+  console.log(`min :: ${min}, max :: ${max}, id_list :: ${id_list}`);
   response.status(200).json({
-    body: request.body
+    id_list: id_list
   });
 }
