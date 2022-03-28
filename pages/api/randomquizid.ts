c220e097db474173cb83b735a138d14b862a14e9
sourabhmandal <19mandal97@gmail.com>
2022-03-28 18:35:57 +0530

    remove useless functions

--- pages/api/randomquizid.ts
+++ pages/api/randomquizid.ts
@@ -1,14 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-
-function max_array(array : ) {
-  return Math.max.apply(null, this);
-}
-
-function min_array(array : [number]) {
-  return Math.min.apply();
-}
-
 export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
@@ -21,7 +12,7 @@ export default function handler(
   for (let i = 0; i < 9; i++) {
     id_list.push(id_array[Math.floor(Math.random() * (max - min + 1)) + min]);
   }
-  
+
   console.log(`min :: ${min}, max :: ${max}, id_list :: ${id_list}`);
   response.status(200).json({
     id_list: id_list
